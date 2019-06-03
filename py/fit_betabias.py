5bd217dd46594f255f87a8457d5f59faec4de3fd
inacflood <iflood@dhcp-10-4-13-126.wireless.rochester.edu>
2019-06-03 17:01:10 -0400

    Update fit_betabias.py

--- py/fit_betabias.py
+++ py/fit_betabias.py
@@ -18,6 +18,11 @@ k_arr = [10**i for i in exp_arr]
 
 def makePower(k,mu,beta,b,z=2.4):
     
+    """
+    Find the value of the 3D power spectrum at k for input values of mu, beta, and bias.
+    The redshift value z can also be adjusted.
+    """
+    
     cosmo = cCAMB.Cosmology(z)
     th = tP3D.TheoryLyaP3D(cosmo)
     power = th.FluxP3D_hMpc(z,k,mu,beta_lya=beta,b_lya=b)
@@ -39,8 +44,13 @@ def makePower(k,mu,beta,b,z=2.4):
 # Make function P(k ; beta, bias)
     
 def makePowerList(k,mu_list,beta,b,z=2.4):
-    power_list = [makePower(k,m,beta,b) for m in mu_list]
-    return power_list
+     """
+     Create a list of the 3D power values corresponding to the values in mu_list, 
+    given values of beta and bias.
+    The redshift value z can also be adjusted.
+     """
+     power_list = [makePower(k,m,beta,b) for m in mu_list]
+     return power_list
 
 # Test function makePowerList
 #mu_list=np.linspace(0.,1.,10)
@@ -55,6 +65,11 @@ def makePowerList(k,mu_list,beta,b,z=2.4):
 #plt.plot(mu_list,powerList_k3_beta10_b10)
 
 def integPower(k,beta,b,refinement=100,z=2.4):
+    """
+    Do a Riemannian integral of the Power at k from a list of 3D power values for 
+    _refinement_ mu points given values of beta and bias.
+    The redshift value z can also be adjusted.
+    """
     mu_list = np.linspace(0.,1.,refinement)
     power_list = makePowerList(k,mu_list,beta,b,z)
     #print(power_list)
@@ -67,6 +82,12 @@ def integPower(k,beta,b,refinement=100,z=2.4):
 
 
 def makePofk(beta,b,kRefine=100,muRefine=100,z=2.4):
+    """
+    Using a table of k_Refine integrated power spectrum values (with dmu in integration 
+    given by muRefine) create a function P(k) using interp1D, with values for beta 
+    and bias given.
+    The redshift value z can also be adjusted.
+    """
     exp_list = np.linspace(-4.,0.9,kRefine)
     k_list = [10**i for i in exp_list]
     P_list = [integPower(k,beta,b,muRefine,z) for k in k_list]
@@ -74,7 +95,7 @@ def makePofk(beta,b,kRefine=100,muRefine=100,z=2.4):
     return Pofk
 
 # Test function makePofk
-Pofk_beta10_b10=makePofk(1.0,-0.1,kRefine=10,muRefine=10)
+Pofk_beta10_b10=makePofk(1.0,-0.1,kRefine=1000,muRefine=1000)
 print(Pofk_beta10_b10(0.12))
 k_new = np.linspace(10**(-4),0.9,50)
 P_funcTest = [Pofk_beta10_b10(k) for k in k_new]
