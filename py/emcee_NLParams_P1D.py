065f5622ab7c2822169fb784b5245bc051d657f7
inacflood <iflood@dhcp-10-4-14-239.wireless.rochester.edu>
2019-06-14 10:44:55 -0400

    Updates emcee NLC

--- py/emcee_NLParams_P1D.py
+++ py/emcee_NLParams_P1D.py
@@ -12,21 +12,29 @@ import matplotlib.pyplot as plt
 import scipy.optimize as op
 import corner
 import get_npd_p1d_woFitsio as npd
+import FiducialValues_Arinyo2015 as fv
 #from scipy.stats import norm
 
-z24=2.4
-cosmo24 = cCAMB.Cosmology(z24)
-th24 = tLyA.TheoryLyaP3D(cosmo24)
-dkM24z = th24.cosmo.dkms_dhMpc(z24)
+# Setup initial parameters
+z=2.2
+err = 1.0
+z_str=str(int(z*10))
+err_str = str(int(err*100))
+
+q1_f, q2_f, kp_f, kvav_f, av_f, bv_f = fv.getFiducialValues(z)
+
+cosmo = cCAMB.Cosmology(z)
+th = tLyA.TheoryLyaP3D(cosmo)
+dkMz = th.cosmo.dkms_dhMpc(z)
 
 # Get actual data
-data = npd.LyA_P1D(2.4)
+data = npd.LyA_P1D(z)
 k = data.k
-k_res = k*dkM24z
-P = data.k/np.pi*data.Pk_emp()
-Perr = data.Pk_stat*data.k/np.pi
+k_res = k*dkMz  # data rescaled b/c of difference in units
+P = data.Pk_emp()
+Perr = data.Pk_stat 
 
-# Plot our synthetic data along with fit from MLE
+# Plot data 
 fig = plt.figure(1)
 ax = fig.add_subplot()
 plt.xscale('linear')
@@ -34,20 +42,17 @@ plt.yscale('log')
 plt.xlabel(r'$k_{\parallel}\,\left(\rm km/s\right)^{-1}$')
 plt.ylabel(r'$(k_{\parallel}/\pi)*P_{1D}(k_{\parallel})$')
 
-ax.errorbar(k,P,yerr=Perr,fmt='k.')
+ax.errorbar(k,P*k/np.pi,yerr=Perr*k/np.pi,fmt='k.')
 
 
 # Maximum Likelihood Estimate fit to the synthetic data
 
 def lnlike(theta, k_res, P, Perr):
     q1,q2 = theta
-    model = th24.makeP1D_P(k_res, q1=q1, q2=q2)*k_res/np.pi
+    model = th.makeP1D_P(k_res, q1=q1, q2=q2, kvav=kvav_f, kp=kp_f, av=av_f, bv=bv_f)*dkMz
     inv_sigma2 = 1.0/(Perr**2 + model**2)
     return -0.5*(np.sum((P-model)**2*inv_sigma2))
 
-err = 0.1
-q1_f = 0.057 
-q2_f = 0.368
 var_1=np.abs(q1_f)*err
 var_2=np.abs(q2_f)*err
 min_1= q1_f - var_1
@@ -62,9 +67,11 @@ nll = lambda *args: -lnlike(*args)
 result = op.minimize(nll, [q1_f, q2_f], args=(k_res, P, Perr),method='L-BFGS-B',bounds=[(min_1,max_1),(min_2,max_2)])
 q1_ml, q2_ml = result["x"]
 
-result_plot = th24.makeP1D_P(k_res, q1=q1_ml, q2=q2_ml)*k_res/np.pi
+result_plot = th.makeP1D_P(k_res, q1=q1_ml, q2=q2_ml, kvav=kvav_f, av=av_f, bv=bv_f)*k_res/np.pi
+fake_plot = th.makeP1D_P(k_res, q1=q1_f, q2=q2_f,kvav=kvav_f,kp=kp_f, av=av_f,bv=bv_f)*k_res/np.pi
+ax.plot(k,fake_plot)
 ax.plot(k,result_plot)
-fig.savefig("../Figures/z24_MCMC_NLParams/{q1,q2}/IntitalFit_error10.pdf")
+fig.savefig("../Figures/MCMC_NLParams_2/q1_q2/z"+z_str+"/IntitalFit_err"+err_str+".pdf")
 #print(b_ml, betap_ml)
 
 
@@ -83,7 +90,7 @@ def lnprob(theta, k, P, Perr):
         return -np.inf
     return lp + lnlike(theta, k, P, Perr)
 
-ndim, nwalkers = 2, 100
+ndim, nwalkers = 2, 200
 pos = [result["x"] + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 
 # Run emcee error evaluation
@@ -96,53 +103,58 @@ chain = sampler.chain
 
 # Plots to visualize emcee walker paths parameter values
 param1 = plt.figure(2)
-plt.ylabel('beta')
-for w in range(nwalkers):
-    plt.plot([chain[w][s][1] for s in range(nsteps)])
-    
-param1.show()
-param1.savefig("../Figures/z24_MCMC_NLParams/{q1,q2}/WalkerPathsq1_error10.pdf")
-
-param2 = plt.figure(3)
-plt.ylabel('b(1+beta)')
+plt.ylabel('q1')
 for w in range(nwalkers):
     plt.plot([chain[w][s][0] for s in range(nsteps)])
     
+param1.show()
+param1.savefig("../Figures/MCMC_NLParams_2/q1_q2/z"+z_str+"/WalkerPathsq1_err"+err_str+".pdf")
+
+param2 = plt.figure(3)
+plt.ylabel('q2')
+for w in range(nwalkers):
+    plt.plot([chain[w][s][1] for s in range(nsteps)])
+    
 param2.show()
-param2.savefig("../Figures/z24_MCMC_NLParams/{q1,q2}/WalkerPathsq2_error10.pdf")
+param2.savefig("../Figures/MCMC_NLParams_2/q1_q2/z"+z_str+"/WalkerPathsq2_err"+err_str+".pdf")
 
 
 
-samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+samples = sampler.chain[:, 100:, :].reshape((-1, ndim))
 
-# Plot a few paths against data and intial fit
+# Plot a few paths against data and fiducial fit
 pathView = plt.figure(4)
 
 for q1, q2 in samples[np.random.randint(len(samples), size=80)]:
-    plt.plot(k, th24.makeP1D_P(k_res, q1=q1, q2=q2)*k_res/np.pi, color="k", alpha=0.1)
-plt.plot(k,th24.makeP1D_P(k_res, q1=q1_f, q2=q2_f)*k_res/np.pi, color="r", lw=2, alpha=0.8)
-plt.errorbar(k, P, yerr=Perr, fmt=".k")
+    plt.plot(k, th.makeP1D_P(k_res, q1=q1, q2=q2, kvav=kvav_f, kp=kp_f, av=av_f, bv=bv_f)*k_res/np.pi, color="k", alpha=0.1)
+plt.plot(k,th.makeP1D_P(k_res, q1=q1_f, q2=q2_f, kvav=kvav_f, kp=kp_f, av=av_f, bv=bv_f)*k_res/np.pi, color="r", lw=2, alpha=0.8)
+plt.errorbar(k, P*k/np.pi, yerr=Perr*k/np.pi, fmt=".k")
 
 plt.yscale('log')
 plt.xlabel('k [(Mpc/h)^-1]')
 plt.ylabel('P(k)*k/pi')
 plt.title('Parameter exploration for beta, bias')
 
-pathView.savefig("../Figures/z24_MCMC_NLParams/{q1,q2}/SamplePaths_error10.pdf")
+pathView.savefig("../Figures/MCMC_NLParams_2/q1_q2/z"+z_str+"/SamplePaths_err"+err_str+".pdf")
 pathView.show()
 
 # Final results
-cornerplt = corner.corner(samples, labels=["$bp$", "$beta$"],
+cornerplt = corner.corner(samples, labels=["$q1$", "$q2$"],
                       truths=[q1_f, q2_f],quantiles=[0.16, 0.5, 0.84],show_titles=True)
-cornerplt.savefig("../Figures/z24_MCMC_NLParams/{q1,q2}/triangle_error10.png")
+cornerplt.savefig("../Figures/MCMC_NLParams_2/q1_q2/z"+z_str+"/triangle_err"+err_str+".pdf")
 cornerplt.show()
 
 
-#samples[:, 1] = np.exp(samples[:, 1])
-#b_mcmc, betap_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-#                             zip(*np.percentile(samples, [16, 50, 84],
-#                                                axis=0)))
-#print("b:", b_mcmc, "beta:", [betaConvert(betap,b_mcmc[1],mu) for betap in betap_mcmc])
+q1_mcmc, q2_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+                            zip(*np.percentile(samples, [16, 50, 84],
+                                                axis=0)))
+file = open("../Figures/MCMC_NLParams_2/q1_q2/z"+z_str+"_q1_q2.txt","a+") 
+ 
+file.write('Prior width {0}, q1 {1}, q2 {2}.\n'.format(str(err), str(q1_mcmc),
+                                                       str(q2_mcmc))) 
+file.close() 
+
+print("q1:", q1_mcmc, "q2:", q2_mcmc)
 
 
 
