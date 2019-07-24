d309e20e78340e41059bb7b5ca96c23f1f34aa12
inacflood <iflood@dhcp-10-5-23-127.wireless.rochester.edu>
2019-07-24 13:54:18 -0400

    Update MC_NLC_P1D_3param_pool.py

--- py/MC_NLC_P1D_3param_pool.py
+++ py/MC_NLC_P1D_3param_pool.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     # Choose the "true" parameters.
     q1_f, q2_f, kp_f, kvav_f, av_f, bv_f = getFiducialValues(z)
-    fidList = [kp_f, kvav_f, bv_f] #manual change line
+    fidList = [kp_f, kvav_f, av_f] #manual change line
     fids = len(fidList)
     
     #q1_e = 0.46008
@@ -88,8 +88,8 @@ if __name__ == '__main__':
     # Maximum Likelihood Estimate fit to the synthetic data
     
     def lnlike(theta):
-        kp,kvav,bv = theta #manual change line
-        model = th.FluxP1D_hMpc(z, k*dkMz, q1=q1_f, q2=q2_f, kp=kp, kvav=kvav, av=av_f, bv=bv)*dkMz #manual change line
+        kp,kvav,av = theta #manual change line
+        model = th.FluxP1D_hMpc(z, k*dkMz, q1=q1_f, q2=q2_f, kp=kp, kvav=kvav, av=av, bv=bv_f)*dkMz #manual change line
         inv_sigma2 = 1.0/(Perr**2)
         return -0.5*(np.sum((P-model)**2*inv_sigma2))
     
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     #    var_list[num] = var
     
     min_list = [0,0,0] #manual change line
-    max_list = [25,2,5] #manual change line
+    max_list = [25,2,2] #manual change line
     
     # Set up MLE for emcee error evaluation
     
